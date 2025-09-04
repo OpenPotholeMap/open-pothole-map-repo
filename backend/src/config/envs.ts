4dac4e0e497f4cb3be8140aca4570d24426096ac
QuangMinhNguyen27405 <quangminh27405@gmail.com>
2025-09-04 15:27:28 -0400

    Set up server and env variables

--- backend/src/config/envs.ts
+++ backend/src/config/envs.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 const envConfig = {
-    port: undefined,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 8000,
 }
 
 export default envConfig;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import envConfig from './config/envs.js';
 
 const app = express();
 
+// Simple CORS setup
+app.use(cors());
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Start server
 app.listen(envConfig.port, () => {
-    console.log(`Server is running on port ${envConfig.port}`);
-})
+  console.log(`Server running on port ${envConfig.port}`);
+});
