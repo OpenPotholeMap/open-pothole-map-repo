124b28c4d56f1be21845522c8a1aec9f7c1f9afc
QuangMinhNguyen27405 <quangminh27405@gmail.com>
2025-09-27 23:04:52 -0400

    Add credential báe64

--- backend/src/config/firebase.ts
+++ backend/src/config/firebase.ts
@@ -1,10 +1,18 @@
 import { initializeApp, applicationDefault, cert } from "firebase-admin/app";
 import { FIREBASE_APPLICATION_CREDENTIALS } from "./envs";
 
+let credentials;
+
+if (FIREBASE_APPLICATION_CREDENTIALS) {
+  const decoded = Buffer.from(
+    FIREBASE_APPLICATION_CREDENTIALS,
+    "base64"
+  ).toString("utf8");
+  credentials = JSON.parse(decoded);
+}
+
 const firebaseApp = initializeApp({
-  credential: FIREBASE_APPLICATION_CREDENTIALS
-    ? cert(FIREBASE_APPLICATION_CREDENTIALS)
-    : applicationDefault()
+  credential: credentials ? cert(credentials) : applicationDefault(),
 });
 
 export default firebaseApp;
